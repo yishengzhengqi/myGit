c81b46fa690a5647e9b8c6fb3136d56d1cf40f79
phj <2602688233@qq.com>
2022-04-09 14:59:27 +0800

    six commit

---
+++ src/main/java/com/phj/pojo/Man.java
@@ -0,0 +1,18 @@
+package com.phj.pojo;
+
+/**
+ * @ClassName Man
+ * @Description
+ * @Author ppp
+ * @Date 2022-04-09 14:55
+ */
+public class Man implements person {
+    @Override
+    public int getAge() {
+        return 0;
+    }
+
+    public static void main(String[] args) {
+        System.out.println("good");
+    }
+}
--- src/main/java/com/phj/pojo/person.java
+++ src/main/java/com/phj/pojo/person.java
@@ -1,4 +1,4 @@
-package main.com.phj.pojo;
+package com.phj.pojo;
 
 /**
  * @ClassName person
