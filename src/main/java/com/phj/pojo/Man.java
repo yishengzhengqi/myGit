741f72d550c59341932b6e761d517259ed6fb13e
phj <2602688233@qq.com>
2022-04-09 14:41:08 +0800

    fourth commit

--- src/main/java/com/phj/pojo/Man.java
+++ src/main/java/com/phj/pojo/Man.java
@@ -1,4 +1,4 @@
-package com.phj.pojo;
+package main.java.com.phj.pojo;
 
 /**
  * @ClassName Man
@@ -6,10 +6,14 @@ package com.phj.pojo;
  * @Author ppp
  * @Date 2022-04-09 14:24
  */
-public class Man implements person{
+public class Man implements com.phj.pojo.person {
 
     @Override
     public int getAge() {
         return 21;
+    }
+
+    public static void main(String[] args) {
+        System.out.println("zhang3");
     }
 }
