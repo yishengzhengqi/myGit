25d48b787d60c3b88cfaafe00fd86435ec31e4f4
phj <2602688233@qq.com>
2022-04-09 14:23:04 +0800

    first commit

---
+++ src/com/phj/pojo/person.java
@@ -0,0 +1,17 @@
+package com.phj.pojo;
+
+/**
+ * @ClassName person
+ * @Description TODO
+ * @Author zenghw
+ * @Date 2022-04-09 14:20
+ */
+public interface person {
+
+    int age = 0;
+    String name = "";
+
+    int getAge();
+
+    String getName();
+}
